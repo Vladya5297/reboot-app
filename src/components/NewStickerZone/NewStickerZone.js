4d0445b83cf5fac2da8a49f2e8476d656af5bb68
Vladya <123dobro@mail.ru>
2019-11-04 21:28:51 +0300

    Minor changes

--- src/components/NewStickerZone/NewStickerZone.js
+++ src/components/NewStickerZone/NewStickerZone.js
@@ -5,7 +5,12 @@ import * as style from './NewStickerZone.module.css'
 const NewStickerZone = (props)=>{
     return (
         <div className={style.wrapper}>
-            {props.sticker && <Sticker id={props.sticker.id} type={props.sticker.type}/>}
+            {props.sticker && <Sticker 
+            header={props.sticker.header}
+            content={props.sticker.content}
+            position={0}
+            id={props.sticker.id} 
+            type={props.sticker.type}/>}
         </div>
     );
 }
