27ef77568926c83d6cc69276774ebb1f2e7683ca
kirill9875 <kirill1098@gmail.com>
2019-11-09 16:42:08 +0300

    Added lib for tooltip and delete last css tooltip. Now we use tooltip into Lib @material UI. Check comments

--- src/components/SegmentHeader/SegmentHeader.js
+++ src/components/SegmentHeader/SegmentHeader.js
@@ -4,10 +4,11 @@ import HelpIcon from '../HelpIcon/HelpIcon'
 import typeProperties from '../../store/typeProperties'
 
 const SegmentHeader = (props) => {
+    console.log(typeProperties[props.type].helptext)
     return (
         <div className={style.wrapper}>
             {typeProperties[props.type].title}
-            <HelpIcon helpText={typeProperties[props.type].helpText} />
+            <HelpIcon helpText={typeProperties[props.type].helptext} />
         </div>
     )
 }
--- src/store/typeProperties.js
+++ src/store/typeProperties.js
@@ -1,10 +1,10 @@
 export default {
-    benefits: {color: "#AFFFC7", title: "Выгода", helptext: "", slots: 4},
-    advantages: {color: "#FF8BF0", title: "Преимущества", helptext: "", slots: 4},
-    problemSolvings: {color: "#AFE7FF", title: "Как продукт решает проблему", helptext: "", slots: 8},
-    desires: {color: "#FA6980", title: "Желания", helptext: "", slots: 4},
-    solutions: {color: "#52E5BA", title: "Решения", helptext: "", slots: 4},
-    tasks: {color: "#72ACFF", title: "Задачи", helptext: "", slots: 4},
-    problems: {color: "#FFC96C", title: "Проблемы", helptext: "", slots: 4},
+    benefits: {color: "#AFFFC7", title: "Выгода", helptext: "HelloWorld6", slots: 4},
+    advantages: {color: "#FF8BF0", title: "Преимущества", helptext: "HelloWorld5", slots: 4},
+    problemSolvings: {color: "#AFE7FF", title: "Как продукт решает проблему", helptext: "HelloWorld4", slots: 8},
+    desires: {color: "#FA6980", title: "Желания", helptext: "HelloWorld3", slots: 4},
+    solutions: {color: "#52E5BA", title: "Решения", helptext: "HelloWorld2", slots: 4},
+    tasks: {color: "#72ACFF", title: "Задачи", helptext: "HelloWorld1", slots: 4},
+    problems: {color: "#FFC96C", title: "Проблемы", helptext: " HelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg dHelloWorld fgdgdfgdfg dgdgdgdg dg + fgdgdfgdfg dgdgdgdg dg dg dg d gd g dg dg d gd g dg d gd g dg dg d gd ", slots: 4},
     newSticker: {color: "#ffffaf", title: "", helptext: "", slots: 4}
 }
