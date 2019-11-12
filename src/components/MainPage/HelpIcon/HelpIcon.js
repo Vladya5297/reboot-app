29788edad4b713538f72b132b4806214da402c47
Vladya <123dobro@mail.ru>
2019-11-12 16:31:16 +0300

    New tooltip library

--- src/components/MainPage/HelpIcon/HelpIcon.js
+++ src/components/MainPage/HelpIcon/HelpIcon.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import Tooltip from 'react-tooltip-lite';
+import Tooltip from 'react-png-tooltip';
 import typeProperties from '../../../store/typeProperties'
-import './Tooltip.css'
 import * as style from './HelpIcon.module.css'
 
 const HelpIcon = (props) => {
     return (
-        <Tooltip content={typeProperties[props.type].helptext}>
-            <div className={style.helpIcon} />
+        <Tooltip tooltip={<div className={style.helpIcon} />} className={style.tooltip}>
+            {typeProperties[props.type].helptext}
         </Tooltip>
     )
 }
