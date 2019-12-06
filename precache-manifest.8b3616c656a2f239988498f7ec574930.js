746097f3182e52e660b764a27cf5b6bf1bf2ab85
Vladya5297 <123dobro@mail.ru>
2019-12-06 17:39:23 +0300

    Updates

---
+++ precache-manifest.8b3616c656a2f239988498f7ec574930.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d190cc160e65777b8dea07230b1a2b46",
+    "url": "/CROCapp/index.html"
+  },
+  {
+    "revision": "90279d698d1ef6bb20ab",
+    "url": "/CROCapp/static/css/2.dfe7030c.chunk.css"
+  },
+  {
+    "revision": "825b3ed531968033d87e",
+    "url": "/CROCapp/static/css/main.cabc5919.chunk.css"
+  },
+  {
+    "revision": "90279d698d1ef6bb20ab",
+    "url": "/CROCapp/static/js/2.7e3ac930.chunk.js"
+  },
+  {
+    "revision": "825b3ed531968033d87e",
+    "url": "/CROCapp/static/js/main.7771fa92.chunk.js"
+  },
+  {
+    "revision": "5f9e0d38463d959ec3dc",
+    "url": "/CROCapp/static/js/runtime-main.f577538f.js"
+  },
+  {
+    "revision": "6b28c5938d1e4c3f29ef00f27b021303",
+    "url": "/CROCapp/static/media/Circe-Regular.6b28c593.ttf"
+  },
+  {
+    "revision": "40000a14460bf333e592b0641e83b6f5",
+    "url": "/CROCapp/static/media/CloseIcon.40000a14.svg"
+  },
+  {
+    "revision": "d5a0fa09fc3779b5acc26697f85116a4",
+    "url": "/CROCapp/static/media/EditIcon.d5a0fa09.svg"
+  },
+  {
+    "revision": "b40c2fafd84307d2bb9830bcc29d896a",
+    "url": "/CROCapp/static/media/Logo.b40c2faf.svg"
+  },
+  {
+    "revision": "fff4a55f989fba0b6c2c6d22b2a84ed5",
+    "url": "/CROCapp/static/media/QuestionMarkIcon.fff4a55f.svg"
+  },
+  {
+    "revision": "b6b71c639a88bd8ab9ae487112593cd4",
+    "url": "/CROCapp/static/media/TrashIcon.b6b71c63.svg"
+  },
+  {
+    "revision": "6afa5d221cc67aa47f38b01001711bae",
+    "url": "/CROCapp/static/media/timeline1.6afa5d22.svg"
+  },
+  {
+    "revision": "bb43d72ae5ddb18cf4e9a0c003950eb6",
+    "url": "/CROCapp/static/media/timeline2.bb43d72a.svg"
+  }
+]);
