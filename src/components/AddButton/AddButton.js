3380325e7a52d61a28bedeb9b7c8670e5a39827e
Vladya <123dobro@mail.ru>
2019-12-20 04:27:30 +0300

    New Add button behavior

--- src/components/AddButton/AddButton.js
+++ src/components/AddButton/AddButton.js
@@ -5,13 +5,17 @@ import typeProperties from '../../store/typeProperties'
 const AddButton = (props) => {
     const color = typeProperties[props.type].color;
     const style = {
-        backgroundColor: [color]
+        backgroundColor: color
     }
     return (
         <div className={classes.wrapper}
-        onClick={()=>{props.addSticker(props.type)}}
+        onClick={()=>{
+            props.addSticker(props.type);
+            console.log(props.currentId);
+            props.openStickerEditingWindow(props.currentId + 1, props.type);
+        }}
             style={{
-                borderColor: [color],
+                borderColor: color,
                 visibility: props.isVisible ? "visible" : "hidden"
             }}>
             <div className={classes.horisontal} style={style} />
--- src/components/AddButton/AddButtonContainer.js
+++ src/components/AddButton/AddButtonContainer.js
@@ -1,13 +1,16 @@
 import AddButton from './AddButton'
 import { connect } from 'react-redux'
 import { addSticker } from '../../store/actionCreators';
+import { openStickerEditingWindow } from '../../store/actionCreators'
 
 const mapStateToProps = (state, ownProps) => ({
-    isVisible: state.hover === ownProps.type
+    isVisible: state.hover === ownProps.type,
+    currentId: state.stickers.currentId
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    addSticker: (stickerType)=>{dispatch(addSticker(stickerType))}
+    addSticker: (stickerType)=>{dispatch(addSticker(stickerType))},
+    openStickerEditingWindow: (id, stickerType) => dispatch(openStickerEditingWindow(id, stickerType)),
 })
 
 export default connect(
